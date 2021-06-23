ea1f2e7dfe7fd4a855682b8ec81ef28a06c781d6
Lucas Lopes <dev.thuram@gmail.com>
2021-06-23 10:04:49 -0300

    crete form component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import './App.scss'
 
 import { Layout } from 'components/Layout'
 import { Header } from 'components/Header'
+import { Form } from 'components/Form'
 
 function App() {
   return (
     <Layout>
       <div className="container">
         <Header />
+
+        <Form />
       </div>
     </Layout>
   )
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './styles.scss'
+
+export const Form = () => (
+  <form className="form-container">
+    <input type="text" placeholder="username" />
+    <button type="submit">Find profile</button>
+  </form>
+)
